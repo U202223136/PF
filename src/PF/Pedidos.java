7ac2ad1f68e78a1eb02c95a50c07662532a8f763
U202223136 <U202223136@upc.edu.pe>
2022-09-25 20:02:21 -0500

    Agregar funcion de Registro de pedidos

--- src/PF/Pedidos.java
+++ src/PF/Pedidos.java
@@ -1,16 +1,25 @@
 package PF;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pedidos {
     static final String title = "Almacen - OEFA !";
     static final String[] PRODUCTOS = {"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"};
     static int[] STOCKS = {12, 25, 55, 4, 3, 8, 9, 5, 32, 22};
+
+    static String[] PEDIDOS = new String[0];
+    static String[] DETALLES = new String[0];
+
+    static String[] AUX_DET_PROD = new String[0];
+    static int[] AUX_DET_CANT = new int[0];
+    static int[] AUX_PROD_INDEX = new int[0];
+
     public static void main(String[] args) {
 
         //Menu();
-        MovimientosStock();
-
+        //MovimientosStock();
+        IngresePedido();
     }
 
     public static String Menu() {
@@ -39,6 +48,112 @@ public class Pedidos {
         System.out.println();
 
         return "M";
+    }
+
+
+    public static String IngresePedido() {
+        Scanner sc = new Scanner(System.in);
+        String res = "M";
+
+        String codigo;
+        int cantidad;
+
+        System.out.println(title);
+        System.out.print("Ingrese producto : ");
+        codigo = sc.nextLine().toUpperCase();
+
+        System.out.print("Ingrese cantidad : ");
+        cantidad = sc.nextInt();
+
+        int index = -1;
+        boolean productoExiste = false;
+        boolean stockSuficiente = false;
+
+        for (int i = 0; i < PRODUCTOS.length; i++) {
+            if (PRODUCTOS[i].equalsIgnoreCase(codigo)) {
+                index = i;
+                productoExiste = true;
+                stockSuficiente = true;
+                break;
+            }
+        }
+
+        if (!productoExiste) {
+            System.out.println("Codigo no registrado!");
+            System.out.print("Desea ingresar otro codigo Si (S) o Finalizar (F): ");
+            res = sc.next();
+        } else if (STOCKS[index] < cantidad) {
+            stockSuficiente = false;
+            System.out.println("Stocks insuficiente!");
+            System.out.print("Desea ingresar otro codigo Si (S) o Finalizar (F): ");
+            res = sc.next();
+        }
+
+        if (productoExiste && stockSuficiente) {
+            res = AgregarItem(codigo, cantidad, index);
+        }
+
+        if (res.equalsIgnoreCase("F")) res = FinalizarPedido();
+        else if (res.equalsIgnoreCase("S")) res = "P";
+        else res = "M";
+
+        return res;
+    }
+
+    public static String AgregarItem(String codigo, int cantidad, int index) {
+        int _n = AUX_DET_PROD.length;
+
+        AUX_DET_PROD = Arrays.copyOf(AUX_DET_PROD, _n + 1);
+        AUX_DET_CANT = Arrays.copyOf(AUX_DET_CANT, _n + 1);
+        AUX_PROD_INDEX = Arrays.copyOf(AUX_PROD_INDEX, _n + 1);
+
+        AUX_DET_PROD[_n] = codigo;
+        AUX_DET_CANT[_n] = cantidad;
+        AUX_PROD_INDEX[_n] = index;
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Desea agregar nuevo producto? Si (S) o Finalizar (F): ");
+
+        return scn.next();
+    }
+
+    public static String FinalizarPedido() {
+        if (AUX_DET_PROD.length > 0) {
+            int _n = PEDIDOS.length;
+            PEDIDOS = Arrays.copyOf(PEDIDOS, _n + 1);
+            DETALLES = Arrays.copyOf(DETALLES, _n + 1);
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            PEDIDOS[_n] = String.format("P%05d\t|%d", (_n + 1), AUX_DET_PROD.length);
+
+            for (int i = 0; i < AUX_DET_PROD.length; i++) {
+                sBuilder.append(String.format("%d\t |%s\t\t|%d", i + 1, AUX_DET_PROD[i], AUX_DET_CANT[i]));
+
+                if (i + 1 < AUX_DET_PROD.length)
+                    sBuilder.append("@");
+
+                STOCKS[AUX_PROD_INDEX[i]] = STOCKS[AUX_PROD_INDEX[i]] - AUX_DET_CANT[i];
+            }
+
+            DETALLES[_n] = sBuilder.toString();
+
+            System.out.println("Solicitud registrado !");
+            System.out.println();
+
+            NotificarAlAlmacen();
+        }
+
+        AUX_DET_PROD = new String[0];
+        AUX_DET_CANT = new int[0];
+        AUX_PROD_INDEX = new int[0];
+
+        return "M";
+    }
+
+    public static void NotificarAlAlmacen() {
+        System.out.println("Notificando al almacen...");
+        System.out.println();
     }
 
 }
