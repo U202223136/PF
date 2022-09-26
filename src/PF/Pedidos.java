7404edd8a903a4ceff69f916773c582e482463d4
U202223136 <U202223136@upc.edu.pe>
2022-09-25 19:55:08 -0500

    Funcion menu

--- src/PF/Pedidos.java
+++ src/PF/Pedidos.java
@@ -1,10 +1,29 @@
 package PF;
 
+import java.util.Scanner;
+
 public class Pedidos {
+    static final String title = "Almacen - OEFA !";
 
     public static void main(String[] args) {
 
-        System.out.println("Proyecto final - UPC");
+        Menu();
+
     }
+
+    public static String Menu() {
+        Scanner sc = new Scanner(System.in);
+        String res;
+
+        System.out.println(title);
+        System.out.println("================================================================");
+        System.out.println("Stock (K)\t|Nuevo pedido (P)\t|Reporte Pedidos (R)\t|Salir (S)");
+        System.out.print("Seleccione una opcion: ");
+
+        res = sc.nextLine();
+
+        return res;
+    }
+
 
 }
