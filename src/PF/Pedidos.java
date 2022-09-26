0ecee1ea5a888a4667700824ed9c9df6578dc004
U202223136 <U202223136@upc.edu.pe>
2022-09-25 19:58:22 -0500

    Funcion MovimientosStock

--- src/PF/Pedidos.java
+++ src/PF/Pedidos.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Pedidos {
     static final String title = "Almacen - OEFA !";
-
+    static final String[] PRODUCTOS = {"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"};
+    static int[] STOCKS = {12, 25, 55, 4, 3, 8, 9, 5, 32, 22};
     public static void main(String[] args) {
 
-        Menu();
+        //Menu();
+        MovimientosStock();
 
     }
 
@@ -25,5 +27,18 @@ public class Pedidos {
         return res;
     }
 
+    public static String MovimientosStock() {
+        System.out.println(title);
+        System.out.println("Movimientos - Stock");
+        System.out.println("================================================================");
+        System.out.println("Codigo\t\t|Stock");
+        for (int i = 0; i < PRODUCTOS.length; i++) {
+            System.out.printf("%s  \t\t|%d \n", PRODUCTOS[i], STOCKS[i]);
+        }
+
+        System.out.println();
+
+        return "M";
+    }
 
 }
